4314355bfba723cc1a72af949c0acb91f4917ebd
Brian Kim <bkim45606@gmail.com>
2025-06-08 03:39:26 -0500

    added trending movies functionality by connecting project to appwrite

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import Search from './Components/Search.jsx';
 import Loader from './Components/Loader.jsx';
 import MovieCard from './Components/MovieCard.jsx';
 import { useDebounce } from 'react-use';
+import { updateSearchCount } from './appwrite.js';
 
 const API_BASE_URL = "https://api.themoviedb.org/3";
 const API_KEY = import.meta.env.VITE_TMDB_API_KEY;
@@ -52,6 +53,9 @@ function App() {
       }
 
       setMovieList(data.results || []);
+      if (query && data.results.length > 0) {
+        await updateSearchCount(query, data.results[0]);
+      }
     } catch (error) {
 
       console.error(`Error fetching movies: ${error}`);
